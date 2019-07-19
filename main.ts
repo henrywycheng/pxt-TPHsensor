46546b4758da5e5c583e8c5c6694c514092fc2c3
henrywycheng <51484937+henrywycheng@users.noreply.github.com>
2019-07-19 17:02:22 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//% weight=0 color=#3CB371 icon="\uf1b3" block="TPH Sensor"
+//% weight=0 color=#3CB371 icon="\uf1b3" block="tphsensor"
 
 namespace tphsensor {
     /* BME280 TPH sensor addr 0x76 return boolean */
